920bf0af9c0dab5af645d58aa3f3d85253a1523a
Leif Etter <etter.leif@gmail.com>
2025-05-02 23:13:21 +0200

    Improve layout
    * Move content onto repsonsive white rounded box and give shadow
    * Improve Layout Of Sidebar and table
    * Remove background color of body
    * Add indicator for Admin Panel of main content

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,21 @@ import { Sidebar } from "./components/sidebar/Sidebar";
 
 function App() {
   return (
-    <div className="bg-ap-bg text-ap-dark-blue-v2 flex flex-row h-full w-full">
-      <section className="flex-2/6 max-w-xs flex flex-row justify-end">
-        <Sidebar />
-      </section>
-      <section className="text-center flex-4/6 px-5 py-5">
-        <h1>Main Content</h1>
-        <div className="w-full bg-white rounded-2xl shadow-md">
-          <OrderTable orders={genXOrders(50)} />
+    <div className="text-ap-dark-blue-v2 flex flex-col items-center justify-start pt-10 h-full w-full">
+      <button className="bg-white rounded-2xl py-2 px-6 mt-5 text-xl font-semibold shadow-ap-special-shadow">
+        Admin Panel
+      </button>
+      <div className="w-11/12 flex bg-white max-w-7xl rounded-4xl shadow-ap-special-shadow mt-6">
+        <div className="flex-1/6 max-w-xs flex flex-row justify-end">
+          <Sidebar />
         </div>
-      </section>
+        <div className="text-center flex-5/6 px-5 py-5">
+          <h1 className="text-xl font-semibold">Main Content</h1>
+          <div className="w-full bg-white rounded-2xl shadow-md">
+            <OrderTable orders={genXOrders(50)} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
