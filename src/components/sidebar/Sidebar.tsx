f45653efc2e36057d2a5ede40a630a01b8faf0cf
Leif Etter <etter.leif@gmail.com>
2025-05-08 16:56:15 -0400

    add passing routes to sidebar buttons

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -9,16 +9,23 @@ import {
 import { SidebarButton } from "./SidebarButton";
 
 export const Sidebar: React.FC = () => (
-  <aside className="flex flex-col py-8 border-r-ap-light-grey border-r-3 h-full w-full">
-    <h1 className="font-bold text-xl text-center pb-5">Navigation</h1>
-    <nav className="flex flex-col items-center gap-2">
-      <SidebarButton text="Home" icon={faHome} selected={true} />
-      <SidebarButton text="Bestellungen" icon={faTruckFast} />
-      <SidebarButton text="Benutzer" icon={faUser} />
-      <SidebarButton text="Testing" icon={faFlaskVial} />
-      <div className="h-1 w-full bg-ap-bg mt-16 mb-2"></div>
-      <SidebarButton text="Settings" icon={faGear} />
-      <SidebarButton text="Dein Profil" icon={faUserCog} />
+  <aside className="flex flex-col py-8 px-5 border-r-gray-200 border-r-2 h-full w-full">
+    <h1 className="font-bold text-xl pb-8 flex flex-row justify-center">
+      <span>Nav</span>
+      <span className="md:block hidden">igation</span>
+    </h1>
+    <nav className="flex flex-col gap-2 items-start">
+      <SidebarButton route="/home" text="Home" icon={faHome} />
+      <SidebarButton route="/orders" text="Bestellungen" icon={faTruckFast} />
+      <SidebarButton route="/users" text="Benutzer" icon={faUser} />
+      <SidebarButton route="/testing" text="Testing" icon={faFlaskVial} />
+
+      <SidebarButton route="/settings" text="Settings" icon={faGear} />
+      <SidebarButton route="/profile" text="Dein Profil" icon={faUserCog} />
     </nav>
   </aside>
 );
+
+{
+  /* <div className="h-1 w-full bg-ap-bg mt-16 mb-2"></div> */
+}
