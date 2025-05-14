3b6ff70b0429ac6f4a7fe80466e5c48d3d143d27
Leif Etter <etter.leif@gmail.com>
2025-05-13 21:09:39 -0400

    remove logic from __root that is already in _auth

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,24 +1,14 @@
-import { Outlet, createRootRoute, useRouter } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
-import { Sidebar } from "../components/sidebar/Sidebar";
+import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
+import { AuthContextType } from "../providers/authProvider";
 
-export const Route = createRootRoute({
-  component: () => {
-    return (
-      <div className="flex flex-col items-center justify-start pt-10 h-full w-full">
-        <button className="bg-white rounded-2xl py-2 px-6 mt-5 text-xl font-semibold shadow-ap-special-shadow">
-          Admin Panel
-        </button>
-        <div className="w-11/12 flex bg-ap-light-grey max-w-7xl rounded-4xl shadow-ap-special-shadow mt-6">
-          <div className="max-w-xs flex flex-row justify-end">
-            <Sidebar />
-          </div>
-          <div className="flex-5/6">
-            <Outlet />
-          </div>
-        </div>
-        <TanStackRouterDevtools />
-      </div>
-    );
-  },
+interface RouterContext {
+  auth: AuthContextType;
+}
+
+export const Route = createRootRouteWithContext<RouterContext>()({
+  component: () => (
+    <>
+      <Outlet />
+    </>
+  ),
 });
