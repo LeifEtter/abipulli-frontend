54d128600e151bce246ba2b150478bbd9c77f56f
Leif Etter <etter.leif@gmail.com>
2025-05-13 21:05:41 -0400

    Create authProvider for supplying user info and function to upd user info

---
+++ src/providers/authProvider.tsx
@@ -0,0 +1,26 @@
+import { createContext, useCallback, useState } from "react";
+import { User } from "../services/endpoints/user";
+
+export interface AuthContextType {
+  user: Partial<User> | null;
+  login: (user: Partial<User>) => Promise<void>;
+}
+
+export const AuthContext = createContext<AuthContextType | null>(null);
+
+interface AuthProviderProps {
+  children: React.ReactNode;
+}
+
+export const AuthProvider = ({ children }: AuthProviderProps): any => {
+  const [user, setUser] = useState<Partial<User> | null>(null);
+  const login = useCallback(async (user: Partial<User>) => {
+    setUser(user);
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, login }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
