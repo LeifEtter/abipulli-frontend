518133c9889479dd9ee3a82333045f42db557321
Leif Etter <etter.leif@gmail.com>
2025-05-13 21:10:28 -0400

    create router using authContext and use provider properly

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,17 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { routeTree } from "./routeTree.gen";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { useAuth } from "./hooks/useAuth";
+import { AuthProvider } from "./providers/authProvider";
 
-const router = createRouter({ routeTree });
+export const router = createRouter({
+  routeTree,
+  defaultPreload: "intent",
+  scrollRestoration: true,
+  context: {
+    auth: undefined!,
+  },
+});
 
 declare module "@tanstack/react-router" {
   interface Register {
@@ -12,8 +21,15 @@ declare module "@tanstack/react-router" {
   }
 }
 
+function InnerApp() {
+  const auth = useAuth();
+  return <RouterProvider router={router} context={{ auth }} />;
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <InnerApp />
+    </AuthProvider>
   </StrictMode>
 );
