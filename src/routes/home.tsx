1227229e3c36686e2525e8ca9cf8f372d8630b9e
Leif Etter <etter.leif@gmail.com>
2025-05-13 21:09:59 -0400

    remove old unused pages

--- src/routes/home.tsx
+++
@@ -1,8 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import React from "react";
-
-const Home: React.FC = () => <></>;
-
-export const Route = createFileRoute("/home")({
-  component: Home,
-});
