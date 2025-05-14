665c04985ea856965507ef14fc0b70a54943f5b3
Leif Etter <etter.leif@gmail.com>
2025-05-13 21:09:12 -0400

    improve sidebar logic and styling

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -8,20 +8,49 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { SidebarButton } from "./SidebarButton";
 
-export const Sidebar: React.FC = () => (
+interface SidebarProps {
+  isLoggedIn: boolean;
+}
+
+export const Sidebar: React.FC<SidebarProps> = ({ isLoggedIn }) => (
   <aside className="flex flex-col py-8 px-5 border-r-gray-200 border-r-2 h-full w-full">
     <h1 className="font-bold text-xl pb-8 flex flex-row justify-center">
       <span>Nav</span>
       <span className="md:block hidden">igation</span>
     </h1>
     <nav className="flex flex-col gap-2 items-start">
-      <SidebarButton route="/home" text="Home" icon={faHome} />
-      <SidebarButton route="/orders" text="Bestellungen" icon={faTruckFast} />
+      <SidebarButton
+        route="/home"
+        text="Home"
+        icon={faHome}
+        activated={isLoggedIn}
+      />
+      <SidebarButton
+        route="/orders"
+        text="Bestellungen"
+        icon={faTruckFast}
+        activated={isLoggedIn}
+      />
       <SidebarButton route="/users" text="Benutzer" icon={faUser} />
-      <SidebarButton route="/testing" text="Testing" icon={faFlaskVial} />
+      <SidebarButton
+        route="/testing"
+        text="Testing"
+        icon={faFlaskVial}
+        activated={isLoggedIn}
+      />
 
-      <SidebarButton route="/settings" text="Settings" icon={faGear} />
-      <SidebarButton route="/profile" text="Dein Profil" icon={faUserCog} />
+      <SidebarButton
+        route="/settings"
+        text="Settings"
+        icon={faGear}
+        activated={isLoggedIn}
+      />
+      <SidebarButton
+        route="/profile"
+        text="Dein Profil"
+        icon={faUserCog}
+        activated={isLoggedIn}
+      />
     </nav>
   </aside>
 );
