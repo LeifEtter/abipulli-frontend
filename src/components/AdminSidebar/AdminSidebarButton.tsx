9787eeb66996c483cd6cbf7f3a68ddbb8c317f83
Leif Etter <etter.leif@gmail.com>
2025-06-02 23:38:24 +0200

    move Sidebar into seperate files and move other Sidebar into Admin Sidebar

---
+++ src/components/AdminSidebar/AdminSidebarButton.tsx
@@ -0,0 +1,36 @@
+import { IconDefinition } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link, useRouterState } from "@tanstack/react-router";
+
+export const AdminSidebarButton: React.FC<{
+  route: string;
+  text: string;
+  icon: IconDefinition;
+  selected?: boolean;
+  activated?: boolean;
+}> = ({ text, route, icon, activated }) => {
+  const state = useRouterState();
+  const buttonSelected = state.location.pathname == route;
+
+  return buttonSelected ? (
+    <Link
+      to={route}
+      className={`gap-2 flex flex-row items-center px-3 py-2 rounded-4xl  ${activated ? "hover:cursor-pointer" : "hover:cursor-not-allowed bg-gray-300 text-gray-200"}`}
+    >
+      <FontAwesomeIcon icon={icon} size={"lg"} width={25} />
+      <p className="hidden md:flex text-md justify-start font-semibold">
+        {text}
+      </p>
+    </Link>
+  ) : (
+    <Link
+      to={route}
+      className={`gap-2 flex flex-row items-center px-3 py-2 rounded-4xl  ${activated ? "hover:cursor-pointer" : "hover:cursor-not-allowed bg-gray-300 text-gray-200"}`}
+    >
+      <FontAwesomeIcon icon={icon} size={"lg"} width={25} />
+      <p className="hidden md:flex text-md justify-start font-semibold">
+        {text}
+      </p>
+    </Link>
+  );
+};
--- src/routes/_auth/route.tsx
+++ src/routes/_auth/route.tsx
@@ -1,67 +1,13 @@
-import { IconProp } from "@fortawesome/fontawesome-svg-core";
-import {
-  faCircleQuestion,
-  faInfo,
-  faMessage,
-  faPenRuler,
-  faPoll,
-  faSliders,
-  faUser,
-} from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { createFileRoute, Link, Outlet } from "@tanstack/react-router";
+import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { LeftSideBar } from "src/components/Sidebar/Sidebar";
 
 export const Route = createFileRoute("/_auth")({
   component: RouteComponent,
 });
 
-const Spacer: React.FC = () => <div className="flex flex-grow"></div>;
-
-const Divider: React.FC = () => (
-  <div className="w-10/12 border-2 border-ap-new-light-gray-greenish opacity-50 my-3" />
-);
-
-interface SidebarElementProps {
-  icon: IconProp;
-  route: string;
-}
-
-const SidebarElement = ({ icon, route }: SidebarElementProps) => (
-  <Link to={route}>
-    <FontAwesomeIcon size={"2x"} icon={icon} />
-  </Link>
-);
-
-const LeftSideBar: React.FC = () => (
-  <div className="bg-ap-new-green border-ap-new-gray border-2 flex flex-col min-w-16 w-2/12 max-w-26 items-center py-3">
-    <Link to="/">
-      <h2 className="font-semibold text-xl">
-        Abi
-        <br />
-        pulli
-      </h2>
-    </Link>
-    <Divider />
-    <div className="flex flex-col items-center gap-5 mt-5">
-      <SidebarElement icon={faInfo} route="/" />
-      <SidebarElement icon={faSliders} route="/" />
-      <SidebarElement icon={faPenRuler} route="/" />
-      <SidebarElement icon={faPoll} route="/" />
-    </div>
-    <Spacer />
-    <Divider />
-    <div className="flex flex-col gap-6">
-      <SidebarElement icon={faMessage} route="/" />
-      <SidebarElement icon={faCircleQuestion} route="/" />
-    </div>
-    <Divider />
-    <SidebarElement icon={faUser} route="/" />
-  </div>
-);
-
 function RouteComponent() {
   return (
-    <div className="flex h-full w-full">
+    <div className="flex h-full w-full bg-ap-new-beige">
       <LeftSideBar />
       <div className="flex-5/6">
         <Outlet />
