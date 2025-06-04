da3243dffa646ea456e669ebbd2b24a2368603af
Leif Etter <etter.leif@gmail.com>
2025-06-04 12:18:39 +0200

    add onSelect function to DesignTab Component for handling selection

--- src/components/Designer/DesignTab.tsx
+++ src/components/Designer/DesignTab.tsx
@@ -1,11 +1,17 @@
 interface DesignTabParams {
   image: string;
   selected?: boolean;
+  onSelect: () => void;
 }
 
-export const DesignTab: React.FC<DesignTabParams> = ({ image, selected }) => (
+export const DesignTab: React.FC<DesignTabParams> = ({
+  image,
+  selected,
+  onSelect,
+}) => (
   <button
     className={`w-25 h-25 cursor-pointer border-2 border-ap-new-gray rounded-2xl flex items-center justify-center ${selected ? "bg-ap-new-green" : "bg-ap-new-dark-beige"}`}
+    onClick={() => onSelect()}
   >
     <img src={image} className="h-11/12" alt="" />
   </button>
