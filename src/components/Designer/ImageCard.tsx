7345de3ca2f7681a262c9266665b5ce9cd8438f8
Leif Etter <etter.leif@gmail.com>
2025-06-10 23:02:02 +0200

    add image to ImageCard and onClick Function as Prop

--- src/components/Designer/ImageCard.tsx
+++ src/components/Designer/ImageCard.tsx
@@ -1,7 +1,22 @@
+import { faFileUpload, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 interface ImageCardParams {
   image: string;
+  onClick: () => void;
 }
 
-export const ImageCard: React.FC<ImageCardParams> = () => (
-  <div className="border-2 rounded-md border-ap-new-gray aspect-square w-full h-full"></div>
+export const ImageCard: React.FC<ImageCardParams> = ({ image, onClick }) => (
+  <div
+    onClick={onClick}
+    className="border-2 rounded-md border-ap-new-gray aspect-square w-full h-full"
+  >
+    <img src={image} className="rounded-sm object-cover h-full w-full" />
+  </div>
+);
+
+interface UploadImageCardParams {
+  onClick: () => void;
+}
+
 );
