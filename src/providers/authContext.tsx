08524e4048bcee6029eed99fe86407c254ff9aea
Leif Etter <etter.leif@gmail.com>
2025-06-10 22:57:28 +0200

    define boolean as login result to properly respond

--- src/providers/authContext.tsx
+++ src/providers/authContext.tsx
@@ -3,7 +3,7 @@ import { createContext } from "react";
 import { AuthState } from "./authProvider";
 export interface AuthContextType extends AuthState {
   checkAuth: () => Promise<void>;
-  login: (creds: UserLoginParams) => Promise<void>;
+  login: (creds: UserLoginParams) => Promise<boolean>;
   logout: () => Promise<void>;
 }
 
