c95ce760945910f230443762006803a87c1e3745
Leif Etter <etter.leif@gmail.com>
2025-06-17 23:27:27 +0200

    add htmlFor to MediumLabel and turn h2 into label

--- src/components/Texts/MediumLabel.tsx
+++ src/components/Texts/MediumLabel.tsx
@@ -1,7 +1,11 @@
 interface MediumLabelParams {
   text: string;
+  htmlFor?: string;
 }
 
-export const MediumLabel: React.FC<MediumLabelParams> = ({ text }) => (
-  <h2 className="text-xl font-semibold text-ap-new-black mb-2">{text}</h2>
+export const MediumLabel: React.FC<MediumLabelParams> = ({
+  htmlFor,
+  <label
+    htmlFor={htmlFor}
+  </label>
 );
