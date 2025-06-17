d7479446856dd415acbd3dc9405f26c3545205c3
Leif Etter <etter.leif@gmail.com>
2025-06-17 23:28:34 +0200

    add className to MediumLabel to allow more styling

--- src/components/Texts/MediumLabel.tsx
+++ src/components/Texts/MediumLabel.tsx
@@ -1,11 +1,18 @@
 interface MediumLabelParams {
   text: string;
   htmlFor?: string;
+  className?: string;
 }
 
 export const MediumLabel: React.FC<MediumLabelParams> = ({
+  text,
   htmlFor,
+  className,
+}) => (
   <label
+    className={`text-xl font-semibold text-ap-new-black ${className}`}
     htmlFor={htmlFor}
+  >
+    {text}
   </label>
 );
