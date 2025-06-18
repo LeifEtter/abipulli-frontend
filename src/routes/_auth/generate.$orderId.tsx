37de227de280f68eb9cb6efee3ef3ec4b0e8513a
Leif Etter <etter.leif@gmail.com>
2025-06-18 19:15:41 +0200

    show loading indicator when examples not present

--- src/routes/_auth/generate.$orderId.tsx
+++ src/routes/_auth/generate.$orderId.tsx
@@ -191,25 +191,29 @@ function RouteComponent() {
           </ol>
           <MediumLabel text="Beispiel" className="mt-4 mb-2" />
           <ul className="flex flex-col gap-4">
-            {examples.map((e) => (
-              <li className="flex flex-col font-medium text-lg">
-                <div className="flex flex-row gap-2">
-                  <img src={e.imageUrl} width={125} height={125} />
-                  <p>{e.text}</p>
-                </div>
-                <div className="flex flex-row gap-2 mt-2">
-                  {Object.entries(e.styles).map(
-                    ([k, v]: [k: string, v: boolean]) => (
-                      <div
-                        className={`px-2 border rounded-md ${v ? "bg-green-700 text-white" : "bg-ap-new-dark-beige"}`}
-                      >
-                        {k.capitalize()}
-                      </div>
-                    )
-                  )}
-                </div>
-              </li>
-            ))}
+            {examples ? (
+              examples.map((e) => (
+                <li className="flex flex-col font-medium text-lg">
+                  <div className="flex flex-row gap-2">
+                    <img src={e.imageUrl} width={125} height={125} />
+                    <p>{e.text}</p>
+                  </div>
+                  <div className="flex flex-row gap-2 mt-2">
+                    {Object.entries(e.styles).map(
+                      ([k, v]: [k: string, v: boolean]) => (
+                        <div
+                          className={`px-2 border rounded-md ${v ? "bg-green-700 text-white" : "bg-ap-new-dark-beige"}`}
+                        >
+                          {k.capitalize()}
+                        </div>
+                      )
+                    )}
+                  </div>
+                </li>
+              ))
+            ) : (
+              <>Loading</>
+            )}
           </ul>
         </div>
       </div>
