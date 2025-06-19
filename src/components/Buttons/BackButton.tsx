ba3d2f1a3b45d3bcd52b8ca4fa75b5f9c0798176
Leif Etter <etter.leif@gmail.com>
2025-06-19 14:05:58 +0200

    remove margins from BackButton to allow easier positioning

--- src/components/Buttons/BackButton.tsx
+++ src/components/Buttons/BackButton.tsx
@@ -4,7 +4,7 @@ import { Link } from "@tanstack/react-router";
 
 export const BackButton = ({ to }: { to: string }) => (
   <Link
-    className="bg-ap-new-dark-beige rounded-md px-2 py-1 ml-4 shadow-sm border mt-2 flex items-center border-ap-new-gray hover:scale-105"
+    className="bg-ap-new-dark-beige rounded-md px-2 py-1 ml-4 shadow-sm border flex items-center border-ap-new-gray hover:scale-105"
     to={to}
   >
     <FontAwesomeIcon icon={faArrowLeft} className="mr-2" />
