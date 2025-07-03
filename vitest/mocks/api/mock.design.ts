d1a65d854c2842dc223cdf3faa5405fd23ba156c
Leif Etter <etter.leif@gmail.com>
2025-07-03 18:46:32 +0200

    complete design mock api object and adapt to new naming of design factory

--- vitest/mocks/api/mock.design.ts
+++ vitest/mocks/api/mock.design.ts
@@ -1,14 +1,63 @@
-import { Design } from "abipulli-types";
+import {
+  AddImageToDesignParams,
+  Design,
+  ImageWithPositionAndScale,
+  ManipulateImageInDesignParams,
+} from "abipulli-types";
 import { DesignFactory } from "../data/factory.design";
+import { faker } from "@faker-js/faker";
+import { ImageFactory } from "../data/factory.image";
+import { uniqueId } from "../data/utils";
+
+//TODO LAter use "toBeCalledWith" to compare passed params
 
 export const DesignsApi = {
   retrieveOrderDesigns: async (orderNumber: number): Promise<Design[]> =>
-    DesignFactory.getDesigns({
+    DesignFactory.designs({
       orderId: orderNumber,
       amount: 10,
       customerId: 5,
     }),
   retrieveSingleDesign: async (id: number): Promise<Design> =>
-    DesignFactory.getSingleDesign({ id: id, orderId: 5, customerId: 5 }),
-  
+    DesignFactory.design({ id: id, orderId: 5, customerId: 5 }),
+  retrieveAllImagesForDesign: async (
+    designId: number
+  ): Promise<ImageWithPositionAndScale[]> => {
+    const imageNumber = faker.number.int({ min: 0, max: 5 });
+    const images: ImageWithPositionAndScale[] = [];
+    for (let i = 0; i < imageNumber; i++) {
+      images.push(ImageFactory.positionedImage({}));
+    }
+    return images;
+  },
+  addImageToDesign: async ({
+    designId,
+    imageId,
+    addImageToDesignParams,
+  }: {
+    designId: number;
+    imageId: number;
+    addImageToDesignParams: AddImageToDesignParams;
+  }): Promise<ImageWithPositionAndScale> => ({
+    ...ImageFactory.image({ id: imageId }),
+    imageToDesignId: uniqueId(),
+    ...addImageToDesignParams,
+  }),
+  manipulateImageInDesign: async ({
+    imageToDesignId,
+    designId,
+    manipulateImageParams,
+  }: {
+    imageToDesignId: number;
+    designId: number;
+    manipulateImageParams: ManipulateImageInDesignParams;
+  }): Promise<ImageWithPositionAndScale> => ({
+    ...ImageFactory.image({}),
+    imageToDesignId: imageToDesignId,
+    ...manipulateImageParams,
+  }),
+  removeImageFromDesign: async (
+    image: ImageWithPositionAndScale,
+    designId: number
+  ): Promise<void> => {},
 };
