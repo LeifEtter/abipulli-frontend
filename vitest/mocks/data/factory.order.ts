87e1dddec2c0a839e600e873746ae142ab52aab1
Leif Etter <etter.leif@gmail.com>
2025-07-03 18:46:02 +0200

    add order factory

---
+++ vitest/mocks/data/factory.order.ts
@@ -0,0 +1,36 @@
+import { Order, OrderStatus } from "abipulli-types";
+import { uniqueId } from "./utils";
+import { faker } from "@faker-js/faker";
+
+const orderStatusList: OrderStatus[] = [
+  "BASIC_INFO",
+  "CREATED",
+  "DESIGNING",
+  "FINISHING_INFO",
+  "REVISING",
+];
+
+export const OrderFactory = {
+  order: ({
+    id,
+    customerId,
+    orderStatus,
+  }: {
+    id?: number;
+    customerId?: number;
+    orderStatus?: OrderStatus;
+  }): Order => ({
+    id: id ?? uniqueId(),
+    customerId: customerId ?? uniqueId(),
+    createdAt: faker.date.past(),
+    updatedAt: faker.date.past(),
+    status: orderStatus ?? faker.helpers.arrayElement(orderStatusList),
+    deadline: faker.date.future(),
+    studentAmount: faker.number.int({ min: 20, max: 150 }),
+    school: faker.location.city(),
+    schoolCountry: faker.location.countryCode(),
+    motto: faker.word.noun(),
+    deliveryAddress: faker.location.streetAddress(),
+    billingAddress: faker.location.streetAddress(),
+  }),
+};
