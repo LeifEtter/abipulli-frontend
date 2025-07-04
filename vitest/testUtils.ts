46b49fbc8cef5e04a50a638d5a4306e19fa8dd5d
Leif Etter <etter.leif@gmail.com>
2025-07-04 16:16:26 +0200

    implement function to create mocked File objects

--- vitest/testUtils.ts
+++ vitest/testUtils.ts
@@ -3,3 +3,6 @@ export function delayedResolve<T>(returnValue: T, timeout: number) {
     new Promise<T>((resolve) => setTimeout(() => resolve(returnValue), 10));
 }
 
+export const createMockFile = (buffer: Buffer): Partial<File> => ({
+  size: buffer.length,
+});
