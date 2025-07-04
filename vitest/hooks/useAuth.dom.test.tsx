65c6e0c2bb2c32cac2d34ab3dd6c3f72c6210aaf
Leif Etter <etter.leif@gmail.com>
2025-07-04 14:05:14 +0200

    improve test render layout to be more interactive with test function

--- vitest/hooks/useAuth.dom.test.tsx
+++ vitest/hooks/useAuth.dom.test.tsx
@@ -17,6 +17,24 @@ function TestComponent() {
   const { user, error, isLoading, login, logout } = useAuth();
 
   return (
+    <div>
+      <div data-testid="isLoading">{JSON.stringify(isLoading)}</div>
+      <div data-testid="userData">{JSON.stringify(user)}</div>
+      <div data-testid="error">{JSON.stringify(error)}</div>
+      <button
+        data-testid="login-button"
+        onClick={() => {
+          login({
+            email: "Gandalf.The.Grey@hotmail.com",
+            password: "BilbosGotTheRing123",
+          });
+        }}
+      >
+        Login
+      </button>
+      <button data-testid="logout-button" onClick={logout}>
+        Logout
+      </button>
     </div>
   );
 }
