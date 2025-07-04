748dbdedc662d5a43aa0bd949f02e16a2b990cbe
Leif Etter <etter.leif@gmail.com>
2025-07-04 14:04:40 +0200

    remove previous implementation of mocking and waiting

--- vitest/hooks/useAuth.dom.test.tsx
+++ vitest/hooks/useAuth.dom.test.tsx
@@ -5,7 +5,6 @@ import { UserApi } from "src/api/endpoints/user";
 import { useAuth } from "src/hooks/useAuth";
 import { AuthProvider } from "src/providers/authProvider";
 import { describe, expect, it, vi } from "vitest";
-import { delayedResolve } from "vitest/helpers/helpers";
 
 vi.mock("src/api/endpoints/user", () => ({
   UserApi: {
@@ -17,30 +16,12 @@ vi.mock("src/api/endpoints/user", () => ({
 function TestComponent() {
   const { user, error, isLoading, login, logout } = useAuth();
 
-  useEffect(() => {
-    login({
-      email: "Gandalf.The.Grey@hotmail.com",
-      password: "BilbosGotTheRing123",
-    });
-  }, []);
-
   return (
-    <div data-testid="result">
-      {isLoading ? "Loading" : (error ?? user!.id)}
     </div>
   );
 }
 
 describe("useAuth", () => {
-  it("should test useAuth", async () => {
-    const mockUserId = 42;
-
-    vi.mocked(UserApi.login).mockImplementation(
-      delayedResolve({ id: mockUserId, token: faker.internet.jwt() }, 10)
-    );
-
-    vi.mocked(UserApi.retrieveUserId).mockImplementation(
-      delayedResolve({ id: mockUserId }, 10)
     );
 
     const testComponent = render(
@@ -48,8 +29,6 @@ describe("useAuth", () => {
         <TestComponent />
       </AuthProvider>
     );
-    const textWrapper = await testComponent.findByTestId("result");
-    expect(textWrapper.textContent).toBe("Loading");
     await waitFor(() => {
       expect(testComponent.getByTestId("result").textContent).toBe(
         mockUserId.toString()
