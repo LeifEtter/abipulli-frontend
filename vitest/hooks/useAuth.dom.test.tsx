971e39434fcce8e6dcb0688891d8ae7ed5cf6cfe
Leif Etter <etter.leif@gmail.com>
2025-07-04 13:01:46 +0200

    add test file for testing useAuth hook
    * use timeouts during value resolve to test loading state before  valus
    are retrieved
    * test final state

---
+++ vitest/hooks/useAuth.dom.test.tsx
@@ -0,0 +1,63 @@
+import { faker } from "@faker-js/faker";
+import { render, waitFor } from "@testing-library/react";
+import { useEffect } from "react";
+import { UserApi } from "src/api/endpoints/user";
+import { useAuth } from "src/hooks/useAuth";
+import { AuthProvider } from "src/providers/authProvider";
+import { describe, expect, it, vi } from "vitest";
+
+function delayedResolve<T>(returnValue: T, timeout: number) {
+  return () =>
+    new Promise<T>((resolve) => setTimeout(() => resolve(returnValue), 10));
+}
+
+vi.mock("src/api/endpoints/user", () => ({
+  UserApi: {
+    login: vi.fn(),
+    retrieveUserId: vi.fn(),
+  },
+}));
+
+function TestComponent() {
+  const { user, error, isLoading, login, logout } = useAuth();
+
+  useEffect(() => {
+    login({
+      email: "Gandalf.The.Grey@hotmail.com",
+      password: "BilbosGotTheRing123",
+    });
+  }, []);
+
+  return (
+    <div data-testid="result">
+      {isLoading ? "Loading" : (error ?? user!.id)}
+    </div>
+  );
+}
+
+describe("useAuth", () => {
+  it("should test useAuth", async () => {
+    const mockUserId = 42;
+
+    vi.mocked(UserApi.login).mockImplementation(
+      delayedResolve({ id: mockUserId, token: faker.internet.jwt() }, 10)
+    );
+
+    vi.mocked(UserApi.retrieveUserId).mockImplementation(
+      delayedResolve({ id: mockUserId }, 10)
+    );
+
+    const testComponent = render(
+      <AuthProvider>
+        <TestComponent />
+      </AuthProvider>
+    );
+    const textWrapper = await testComponent.findByTestId("result");
+    expect(textWrapper.textContent).toBe("Loading");
+    await waitFor(() => {
+      expect(testComponent.getByTestId("result").textContent).toBe(
+        mockUserId.toString()
+      );
+    });
+  });
+});
