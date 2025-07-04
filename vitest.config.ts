0d8178109190e6582d33b0d8bbc74db71b14c58a
Leif Etter <etter.leif@gmail.com>
2025-07-04 13:00:59 +0200

    add tsx files to vitest config include

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ import viteConfig from "./vite.config";
 export default defineConfig({
   ...viteConfig,
   test: {
-    include: ["vitest/**/*.test.ts"],
+    include: ["vitest/**/*.test.ts", "vitest/**/*.test.tsx"],
     environment: "jsdom",
     globals: true,
   },
