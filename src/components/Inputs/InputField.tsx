f7b40bf1493c06e37e2390162a060a2d9d3e6ac6
Leif Etter <etter.leif@gmail.com>
2025-07-07 17:20:44 +0200

    implement required prop into InputField with colored asterisk

--- src/components/Inputs/InputField.tsx
+++ src/components/Inputs/InputField.tsx
@@ -10,6 +10,8 @@ interface InputFieldProps {
   multiline?: boolean;
   minLines?: number;
   id?: string;
+  required?: boolean;
+  requiredStarColor?: string;
 }
 
 export const InputField = ({
@@ -20,26 +22,49 @@ export const InputField = ({
   multiline,
   minLines,
   id,
+  required = false,
+  requiredStarColor,
 }: InputFieldProps) => {
   return (
     <div className="flex flex-col">
-      {label ? <SmallLabel htmlFor={`input-${label}`} text={label} /> : <></>}
+      {label ? (
+        <span className="flex gap-1">
+          <SmallLabel
+            htmlFor={`input-${label}`}
+            text={label}
+            className="text-lg font-medium"
+          />
+          {required ? (
+            <p
+              className={`text-lg font-medium ${requiredStarColor ?? "text-black"}`}
+            >
+              *
+            </p>
+          ) : (
+            <></>
+          )}
+        </span>
+      ) : (
+        <></>
+      )}
       {multiline ? (
         <textarea
           id={id ?? `input-${label}`}
-          className="bg-ap-new-dark-beige border-1 border-ap-new-gray rounded-sm py-1 px-3"
+          className="border-1 border-abipulli-grey-border rounded-sm py-1 px-3"
           placeholder={placeholder}
           value={value}
           onChange={onChange}
           rows={minLines}
+          required
         />
       ) : (
         <input
           id={id ?? `input-${label}`}
-          className="bg-ap-new-dark-beige border-1 border-ap-new-gray rounded-sm py-1 px-3 w-full"
+          className="border-1 border-abipulli-grey-border rounded-sm py-1.5 px-3 w-full"
           placeholder={placeholder}
           value={value}
           onChange={onChange}
+          required
         />
       )}
     </div>
