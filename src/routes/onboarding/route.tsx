ba2d881b3062e59a6f692c1275420eca7b256b6e
Leif Etter <etter.leif@gmail.com>
2025-07-07 22:01:28 +0200

    improve onboarding main route layout

--- src/routes/onboarding/route.tsx
+++ src/routes/onboarding/route.tsx
@@ -3,6 +3,7 @@ import { LeftSideBar } from "src/components/Sidebar/Sidebar";
 import TreeBG from "src/assets/tree-bg.png";
 import ThumbsUP from "src/assets/thumbs-up.png";
 import AbipulliMascot from "src/assets/abipulli-mascot.png";
+import { SidebarNew } from "src/components/Sidebar/SidebarNew";
 
 export const Route = createFileRoute("/onboarding")({
   component: RouteComponent,
@@ -11,13 +12,17 @@ export const Route = createFileRoute("/onboarding")({
 function RouteComponent() {
   return (
     <div
-      className="flex h-full w-full bg-abipulli-green-light"
+      className="flex h-full w-full bg-abipulli-green-light pt-30"
       style={{ fontFamily: "Inter" }}
     >
-      <div className="flex-2/12 border"></div>
-      <div className="flex-1/12"></div>
-      <div className="flex-9/12 z-10 pt-52">
-        <Outlet />
+      <div className="ml-0 lg:ml-10 flex-2/12 px-4 z-10">
+        <SidebarNew />
+      </div>
+      <div className="flex-10/12 z-10 flex">
+        <div className="flex-0 lg:flex-0/12 xl:flex-1/12"></div>
+        <div className="flex-12/12">
+          <Outlet />
+        </div>
       </div>
 
       <img className="absolute bottom-0" src={TreeBG} alt="" />
