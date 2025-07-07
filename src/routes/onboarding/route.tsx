0a554ee65bae0ddfa896fcec0e72a0e04bfa3830
Leif Etter <etter.leif@gmail.com>
2025-07-07 17:20:12 +0200

    improve main route of onboarding design with images and layout

--- src/routes/onboarding/route.tsx
+++ src/routes/onboarding/route.tsx
@@ -1,4 +1,9 @@
 import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { LeftSideBar } from "src/components/Sidebar/Sidebar";
+import TreeBG from "src/assets/tree-bg.png";
+import ThumbsUP from "src/assets/thumbs-up.png";
+import AbipulliMascot from "src/assets/abipulli-mascot.png";
+
 export const Route = createFileRoute("/onboarding")({
   component: RouteComponent,
 });
@@ -6,12 +11,26 @@ export const Route = createFileRoute("/onboarding")({
 function RouteComponent() {
   return (
     <div
-      className="flex h-full w-full bg-ap-new-beige"
-      style={{ fontFamily: "Onest" }}
+      className="flex h-full w-full bg-abipulli-green-light"
+      style={{ fontFamily: "Inter" }}
     >
-      <div className="flex-5/6">
-        <div></div>
+      <div className="flex-2/12 border"></div>
+      <div className="flex-1/12"></div>
+      <div className="flex-9/12 z-10 pt-52">
+        <Outlet />
       </div>
+
+      <img className="absolute bottom-0" src={TreeBG} alt="" />
+      <img
+        className="absolute w-70 right-0 bottom-3/12"
+        src={ThumbsUP}
+        alt=""
+      />
+      <img
+        src={AbipulliMascot}
+        className="w-120 absolute right-2/12 -top-1/12"
+        alt=""
+      />
     </div>
   );
 }
