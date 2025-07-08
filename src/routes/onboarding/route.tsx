c700ce2c02f0e57f79d24e773c156ef489c24bfb
Leif Etter <etter.leif@gmail.com>
2025-07-08 23:39:08 +0200

    implement animated routing between onboarding pages
    * fade and swipe up/down

--- src/routes/onboarding/route.tsx
+++ src/routes/onboarding/route.tsx
@@ -1,9 +1,51 @@
 import { createFileRoute, Outlet } from "@tanstack/react-router";
-import { LeftSideBar } from "src/components/Sidebar/Sidebar";
 import TreeBG from "src/assets/tree-bg.png";
 import ThumbsUP from "src/assets/thumbs-up.png";
 import AbipulliMascot from "src/assets/abipulli-mascot.png";
 import { SidebarNew } from "src/components/Sidebar/SidebarNew";
+
+import { AnimatePresence, easeInOut, motion } from "framer-motion";
+import { useLocation } from "@tanstack/react-router";
+import { OnboardingProvider } from "src/providers/onboardingProvider";
+
+import { useEffect, useRef, useState } from "react";
+
+function useNavigationDirection() {
+  const location = useLocation();
+  const prevPath = useRef(location.pathname);
+  const [direction, setDirection] = useState<"forward" | "back">("forward");
+
+  useEffect(() => {
+    if (location.pathname > prevPath.current) {
+      setDirection("forward");
+    } else {
+      setDirection("back");
+    }
+    prevPath.current = location.pathname;
+  }, [location.pathname]);
+
+  return direction;
+}
+
+function AnimatedOutlet() {
+  const location = useLocation();
+
+  const direction = useNavigationDirection();
+
+  return (
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={location.pathname}
+        initial={{ y: direction == "back" ? 800 : -800, opacity: 1 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ y: direction == "back" ? 800 : -800, opacity: 0 }}
+        transition={{ duration: 0.3, ease: easeInOut }}
+      >
+        <Outlet />
+      </motion.div>
+    </AnimatePresence>
+  );
+}
 
 export const Route = createFileRoute("/onboarding")({
   component: RouteComponent,
@@ -19,9 +61,11 @@ function RouteComponent() {
         <SidebarNew />
       </div>
       <div className="flex-10/12 z-10 flex">
-        <div className="flex-0 lg:flex-0/12 xl:flex-1/12"></div>
-        <div className="flex-12/12">
-          <Outlet />
+        <div className="flex-0 xl:flex-1/12"></div>
+        <div className="flex-12/12 xl:flex-11/12">
+          <OnboardingProvider>
+            <AnimatedOutlet />
+          </OnboardingProvider>
         </div>
       </div>
 
