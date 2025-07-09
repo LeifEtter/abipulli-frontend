694fd1afc1705dc527c606143b2f8f862469c5dd
Leif Etter <etter.leif@gmail.com>
2025-07-09 23:54:35 +0200

    add onchange prop to SelectField

--- src/components/Inputs/SelectField.tsx
+++ src/components/Inputs/SelectField.tsx
@@ -8,10 +8,15 @@ interface SelectOption {
 interface SelectFieldProps {
   label: string;
   options: SelectOption[];
+  onChange: (
+    newValue: SingleValue<SelectOption>,
+    actionMeta: ActionMeta<SelectOption>
+  ) => void;
 }
 
 export const SelectField = ({ label, options }: SelectFieldProps) => (
   <div className="flex-1/12 min-w-21">
+  onChange,
     <label htmlFor="select-country-code" className="text-lg font-medium">
       {label} *
     </label>
@@ -19,6 +24,7 @@ export const SelectField = ({ label, options }: SelectFieldProps) => (
       id="select-country-code"
       options={options}
       defaultValue={options[0]}
+      onChange={onChange}
     />
   </div>
 );
