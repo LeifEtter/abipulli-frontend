7a00c56123390f2254cf21d5788167b1b72d9010
Leif Etter <etter.leif@gmail.com>
2025-07-10 22:56:30 +0200

    add account link to sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -4,6 +4,7 @@ import {
   faImages,
   faInfoCircle,
   faLock,
+  faPerson,
   faPoll,
   faShirt,
   faTruckFast,
@@ -11,6 +12,9 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Center } from "../Misc/Center";
 import { Link } from "@tanstack/react-router";
+import { faFaceGrinStars } from "@fortawesome/free-regular-svg-icons";
+import AbiPulliLogo from "src/assets/icons/abipulli-logo.png";
+import Avatar from "src/assets/icons/avatar.png";
 
 interface SidebarTileProps {
   icon: IconProp;
@@ -64,54 +68,65 @@ const SidebarTile = ({
 interface SidebarProps {}
 export const Sidebar: React.FC = () => {
   return (
-    <div className="card flex flex-col items-center p-6 font-semibold group/navbar w-24 lg:w-80 hover:min-w-80">
-      <div className="flex flex-col items-start">
-        <h2 className="text-2xl self-center flex">
-          Nav
-          <span className="hidden lg:block group-hover/navbar:block">
-            igation
-          </span>
-        </h2>
-        <div className="h-4"></div>
-        <SidebarTile
-          icon={faInfoCircle}
-          label="Onboarding"
-          description="AbiPulli Prozess Erklärt"
-          to="onboarding"
-          selected={true}
-        />
-        <SidebarTile
-          icon={faIcons}
-          to="generate"
-          label="Neues Bild Erstellen"
-          description="Generiere neues Element"
-        />
-        <SidebarTile
-          icon={faImages}
-          label="Bild Vorschau"
-          description="Vergleiche Bild Elemente"
-          to="compare"
-        />
-        <SidebarTile
-          icon={faShirt}
-          label="Designer"
-          description="Vergleiche Bild Elemente"
-          to="designer"
-        />
-        <SidebarTile
-          icon={faPoll}
-          label="Umfragetool"
-          description="Starte eine Umfrage"
-          to="polls"
-        />
-        <SidebarTile
-          icon={faTruckFast}
-          label="Bestellabschluss"
-          description="Bestelle deinen AbiPulli"
-          to="order"
-          locked={true}
-        />
+    <div>
+      <div className="card flex flex-col items-center p-6 font-semibold group/navbar w-24 lg:w-80 hover:min-w-80">
+        <div className="flex flex-col items-start">
+          <h2 className="text-2xl self-center flex">
+            Nav
+            <span className="hidden lg:block group-hover/navbar:block">
+              igation
+            </span>
+          </h2>
+          <div className="h-4"></div>
+          <SidebarTile
+            icon={faInfoCircle}
+            label="Onboarding"
+            description="AbiPulli Prozess Erklärt"
+            to="onboarding"
+            selected={true}
+          />
+          <SidebarTile
+            icon={faIcons}
+            to="generate"
+            label="Neues Bild Erstellen"
+            description="Generiere neues Element"
+          />
+          <SidebarTile
+            icon={faImages}
+            label="Bild Vorschau"
+            description="Vergleiche Bild Elemente"
+            to="compare"
+          />
+          <SidebarTile
+            icon={faShirt}
+            label="Designer"
+            description="Vergleiche Bild Elemente"
+            to="designer"
+          />
+          <SidebarTile
+            icon={faPoll}
+            label="Umfragetool"
+            description="Starte eine Umfrage"
+            to="polls"
+          />
+          <SidebarTile
+            icon={faTruckFast}
+            label="Bestellabschluss"
+            description="Bestelle deinen AbiPulli"
+            to="order"
+            locked={true}
+          />
+        </div>
       </div>
+      <Link
+        to={"/onboarding/loginNew"}
+        className="flex flex-row card mt-4 items-center gap-4 p-0.5 justify-start rounded-l-4xl rounded-r-2xl"
+      >
+        <div className="border-2 p-1 rounded-full bg-abipulli-green border-abipulli-offblack">
+          <img src={Avatar} className="w-12" />
+        </div>
+        <span className="text-lg font-semibold">Account</span>
+      </Link>
     </div>
   );
 };
