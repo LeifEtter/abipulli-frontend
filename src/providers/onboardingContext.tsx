ebf878a6771ee1f44c96d249df853d09a6a6304c
Leif Etter <etter.leif@gmail.com>
2025-07-10 22:56:05 +0200

    add error logic to onboarding provider/context

--- src/providers/onboardingContext.tsx
+++ src/providers/onboardingContext.tsx
@@ -1,11 +1,14 @@
 import { OnboardingInfo } from "abipulli-types";
 import { createContext } from "react";
+import { OnboardingErrors } from "./onboardingProvider";
 
 export interface OnboardingContextType extends Partial<OnboardingInfo> {
+  errorState: OnboardingErrors;
+  clearError: (key: keyof OnboardingErrors) => void;
+  setError: ([k, v]: [k: keyof OnboardingErrors, v: string]) => void;
   saveProgressLocally: (state: Partial<OnboardingInfo>) => void;
   submitProgress: () => void;
   getProgress: () => void;
-  setError: () => void;
   saveToLocalStorage: () => void;
   retrieveFromLocalStorage: () => void;
 }
