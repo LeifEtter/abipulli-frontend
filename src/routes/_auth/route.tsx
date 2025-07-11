3df5cb75b14306aad6af2820b3cf2635c2c97bab
Leif Etter <etter.leif@gmail.com>
2025-07-11 18:22:57 +0200

    remove sidebar and styling from _auth/route.tsx to use __root.tsx instead for this

--- src/routes/_auth/route.tsx
+++ src/routes/_auth/route.tsx
@@ -1,20 +1,9 @@
 import { createFileRoute, Outlet } from "@tanstack/react-router";
-import { LeftSideBar } from "src/components/Sidebar/SidebarOld";
 
 export const Route = createFileRoute("/_auth")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return (
-    <div
-      className="flex h-full w-full bg-ap-new-beige"
-      style={{ fontFamily: "Onest" }}
-    >
-      <LeftSideBar />
-      <div className="flex-5/6">
-        <Outlet />
-      </div>
-    </div>
-  );
+  return <Outlet />;
 }
