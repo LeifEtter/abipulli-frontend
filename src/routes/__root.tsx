cbf215f64c03246c938a169889f6e8607d59b1c9
Leif Etter <etter.leif@gmail.com>
2025-07-11 18:15:16 +0200

    move main sidebar setup and background styling from /onboarding/route.tsx to __root.tsx
    * __root.tsx as new point of truth for styling of entire application
    * separate background styling into separate component
    * comment out animation for now

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,14 +1,90 @@
 import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
 import { AuthContextType } from "../providers/authContext";
+import ThumbsUP from "src/assets/thumbs-up.png";
+import TreeBG from "src/assets/tree-bg.png";
+import { AbiPulliLogo } from "src/components/Misc/AbipulliLogo";
+import { SidebarMobile } from "src/components/Sidebar/SidebarMobile";
+import { Sidebar } from "src/components/Sidebar/Sidebar";
 
 interface RouterContext {
   auth: AuthContextType;
 }
 
+const BackgroundImages: React.FC = () => (
+  <div className="[&>*]:absolute w-full h-full ">
+    <img className="bottom-0" src={TreeBG} alt="trees-background" />
+    <img
+      className="w-70 right-0 bottom-3/12"
+      src={ThumbsUP}
+      alt="thumbs-up-img"
+    />
+    {/* <img
+      src={AbipulliMascot}
+      className="w-120 right-2/12 -top-1/12"
+      alt="mascot-img"
+    /> */}
+  </div>
+);
+
 export const Route = createRootRouteWithContext<RouterContext>()({
   component: () => (
-    <>
-      <Outlet />
-    </>
+    <div style={{ fontFamily: "Inter" }}>
+      {/* style={{ fontFamily: "Onest" }} */}
+      <AbiPulliLogo />
+      <div className="flex flex-row mt-16 gap-2 sm:gap-8 lg:gap-12 pl-4 items-start">
+        <div className="block sm:hidden">
+          <SidebarMobile />
+        </div>
+        <div className="hidden sm:block">
+          <Sidebar />
+        </div>
+        <div className="flex-8/12 z-10 flex">
+          <Outlet />
+        </div>
+      </div>
+      <BackgroundImages />
+    </div>
   ),
 });
+
+// function useNavigationDirection() {
+//   const location = useLocation();
+//   const prevPath = useRef(location.pathname);
+//   const [direction, setDirection] = useState<"forward" | "back">("forward");
+
+//   useEffect(() => {
+//     if (location.pathname > prevPath.current) {
+//       setDirection("forward");
+//     } else {
+//       setDirection("back");
+//     }
+//     prevPath.current = location.pathname;
+//   }, [location.pathname]);
+
+//   return direction;
+// }
+
+// function AnimatedOutlet() {
+//   const location = useLocation();
+
+//   const direction = useNavigationDirection();
+
+//   return (
+//     <AnimatePresence mode="wait">
+//       <motion.div
+//         className="w-full h-full"
+//         key={location.pathname}
+//         initial={{ y: direction == "back" ? 800 : -800, opacity: 1 }}
+//         animate={{ y: 0, opacity: 1 }}
+//         exit={{ y: direction == "back" ? 800 : -800, opacity: 0 }}
+//         transition={{ duration: 0.3, ease: easeInOut }}
+//       >
+//         <Outlet />
+//       </motion.div>
+//     </AnimatePresence>
+//   );
+// }
+
+// <OnboardingProvider>
+//   <AnimatedOutlet />
+// </OnboardingProvider>
--- src/routes/onboarding/route.tsx
+++ src/routes/onboarding/route.tsx
@@ -1,103 +1,14 @@
 import { createFileRoute, Outlet } from "@tanstack/react-router";
-import TreeBG from "src/assets/tree-bg.png";
-import ThumbsUP from "src/assets/thumbs-up.png";
-import AbipulliMascot from "src/assets/abipulli-mascot.png";
-import { SidebarNew } from "src/components/Sidebar/SidebarNew";
-
-import { AnimatePresence, easeInOut, motion } from "framer-motion";
-import { useLocation } from "@tanstack/react-router";
 import { OnboardingProvider } from "src/providers/onboardingProvider";
-import AbiPulliLogo from "src/assets/icons/abipulli-logo.png";
-
-import { useEffect, useRef, useState } from "react";
-import { Sidebar } from "src/components/Sidebar/Sidebar";
-import { SidebarMobile } from "src/components/Sidebar/SidebarMobile";
-
-function useNavigationDirection() {
-  const location = useLocation();
-  const prevPath = useRef(location.pathname);
-  const [direction, setDirection] = useState<"forward" | "back">("forward");
-
-  useEffect(() => {
-    if (location.pathname > prevPath.current) {
-      setDirection("forward");
-    } else {
-      setDirection("back");
-    }
-    prevPath.current = location.pathname;
-  }, [location.pathname]);
-
-  return direction;
-}
-
-function AnimatedOutlet() {
-  const location = useLocation();
-
-  const direction = useNavigationDirection();
-
-  return (
-    <AnimatePresence mode="wait">
-      <motion.div
-        className="w-full h-full"
-        key={location.pathname}
-        initial={{ y: direction == "back" ? 800 : -800, opacity: 1 }}
-        animate={{ y: 0, opacity: 1 }}
-        exit={{ y: direction == "back" ? 800 : -800, opacity: 0 }}
-        transition={{ duration: 0.3, ease: easeInOut }}
-      >
-        <Outlet />
-      </motion.div>
-    </AnimatePresence>
-  );
-}
 
 export const Route = createFileRoute("/onboarding")({
   component: RouteComponent,
 });
 
-function RouteComponent() {
+export function RouteComponent() {
   return (
-    <div
-      className="flex flex-col h-full w-full bg-abipulli-green-light"
-      style={{ fontFamily: "Inter" }}
-    >
-      <span className="flex flex-row gap-2 justify-center items-center self-start pt-5 pl-5">
-        <img src={AbiPulliLogo} className="w-8 h-8" />
-        <h2 className="text-xl font-semibold">Abipulli.com</h2>
-      </span>
-      <div className="flex flex-row mt-16 gap-2 sm:gap-8 lg:gap-12 pl-4 items-start">
-        <div className="block sm:hidden">
-          <SidebarMobile />
-        </div>
-        <div className="hidden sm:block">
-          <Sidebar />
-        </div>
-
-        {/* <div className="ml-0 lg:ml-10 basis-72 z-10">
-          <SidebarNew />
-        </div> */}
-        <div className="flex-8/12 z-10 flex">
-          <OnboardingProvider>
-            <AnimatedOutlet />
-          </OnboardingProvider>
-        </div>
-      </div>
-
-      <img
-        className="absolute bottom-0"
-        src={TreeBG}
-        alt="trees-background-image"
-      />
-      <img
-        className="absolute w-70 right-0 bottom-3/12"
-        src={ThumbsUP}
-        alt="thumbs-up-background-image"
-      />
-      <img
-        src={AbipulliMascot}
-        className="w-120 absolute right-2/12 -top-1/12 -z-10"
-        alt="mascot-background-image"
-      />
-    </div>
+    <OnboardingProvider>
+      <Outlet />
+    </OnboardingProvider>
   );
 }
