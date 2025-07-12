47e42abf57f87bea876d29ff499c2d1bbe7ee8b8
Leif Etter <etter.leif@gmail.com>
2025-07-12 17:02:36 +0200

    adapt designtab to new designs component

--- src/components/Designer/DesignTab.tsx
+++ src/components/Designer/DesignTab.tsx
@@ -10,7 +10,7 @@ export const DesignTab: React.FC<DesignTabParams> = ({
   onSelect,
 }) => (
   <button
-    className={`w-25 h-25 cursor-pointer border-2 border-ap-new-gray rounded-2xl flex items-center justify-center ${selected ? "bg-ap-new-green" : "bg-ap-new-dark-beige"}`}
+    className={`cursor-pointer basis-30 aspect-square rounded-2xl flex items-center justify-center ${selected && "bg-abipulli-green border-2"}`}
     onClick={() => onSelect()}
   >
     <img src={image} className="h-11/12" alt="" />
