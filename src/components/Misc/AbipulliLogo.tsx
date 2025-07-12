5e4b5dc37fb5599764b4b7d17e10c2476e89b168
Leif Etter <etter.leif@gmail.com>
2025-07-12 19:41:40 +0200

    improve abipulli logo styling

--- src/components/Misc/AbipulliLogo.tsx
+++ src/components/Misc/AbipulliLogo.tsx
@@ -1,7 +1,7 @@
 import AbiPulli from "src/assets/icons/abipulli-logo.png";
 
 export const AbiPulliLogo: React.FC = () => (
-  <span className="flex flex-row gap-2 justify-center items-center self-start pt-5 pl-5">
+  <span className="flex flex-row gap-2 justify-center items-center pt-6 pl-6">
     <img src={AbiPulli} className="w-8 h-8" />
     <h2 className="text-xl font-semibold">Abipulli.com</h2>
   </span>
