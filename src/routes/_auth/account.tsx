3e24bcc7332153b318b032ff7cc671b6166855d1
Leif Etter <etter.leif@gmail.com>
2025-07-12 17:03:27 +0200

    connect account page with backend; react to responses

--- src/routes/_auth/account.tsx
+++ src/routes/_auth/account.tsx
@@ -1,5 +1,8 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { errorMessages, User } from "abipulli-types";
 import { useEffect, useState } from "react";
+import { ApiError } from "src/api/ApiError";
+import { UserApi } from "src/api/endpoints/user";
 import { BasicButton } from "src/components/Buttons/BasicButton";
 import { InputField } from "src/components/Inputs/InputField";
 import { Divider } from "src/components/Misc/Divider";
@@ -7,6 +10,7 @@ import { PageDescription } from "src/components/Texts/PageDescription";
 import { PageTitle } from "src/components/Texts/PageTitle";
 import { useAuth } from "src/hooks/useAuth";
 import { usePopup } from "src/hooks/usePopup";
+import { useSnackbar } from "src/hooks/useSnackbar";
 
 export const Route = createFileRoute("/_auth/account")({
   component: RouteComponent,
@@ -22,11 +26,20 @@ function RouteComponent() {
   const { user } = useAuth();
 
   const showPopup = usePopup();
+  const showSnackbar = useSnackbar();
 
   const [email, setEmail] = useState<InputInfo>(InputInfoInit);
   const [oldPass, setOldPass] = useState<InputInfo>(InputInfoInit);
   const [newPass, setNewPass] = useState<InputInfo>(InputInfoInit);
-  useEffect(() => {}, []);
+
+  useEffect(() => {
+    const getUserData = async () => {
+      const user: Omit<User, "password"> = await UserApi.getUserData();
+      setEmail((prev) => ({ ...prev, value: user.email }));
+    };
+
+    getUserData();
+  }, [user]);
 
   return (
     <div className="card flex flex-col items-start">
@@ -38,7 +51,7 @@ function RouteComponent() {
       <InputField
         className="mt-4"
         label="Email"
-        value={""}
+        value={email.value ?? ""}
         onChange={(e) =>
           setEmail((prev) => ({ ...prev, value: e.target.value }))
         }
@@ -66,7 +79,37 @@ function RouteComponent() {
           error={newPass.error}
         />
       </div>
-      <BasicButton onClick={() => {}} className="mt-4" shadow>
+      <BasicButton
+        onClick={async () => {
+          try {
+            if (!oldPass.value || !newPass.value) return;
+            await UserApi.changePassword({
+              oldPassword: oldPass.value,
+              password: newPass.value,
+            });
+            showSnackbar({
+              type: "success",
+              message: "Passwort erfolgreich geändert!",
+            });
+          } catch (error) {
+            console.log(error);
+            if (error instanceof ApiError) {
+              if (error.message == errorMessages.faultyLoginCredentials) {
+                setOldPass((prev) => ({
+                  ...prev,
+                  error: "Passwort ist falsch",
+                }));
+              }
+              showSnackbar({
+                type: "error",
+                message: "Ups, etwas ist schiefgelaufen",
+              });
+            }
+          }
+        }}
+        className="mt-4"
+        shadow
+      >
         Passwort Ändern
       </BasicButton>
       <Divider />
