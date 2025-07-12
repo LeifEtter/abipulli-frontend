f5fbe217a3ea3ca62a8ce1a34b3eb067ddd807ec
Leif Etter <etter.leif@gmail.com>
2025-07-12 17:01:11 +0200

    add interceptor to properly throw api erorr responses

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,13 +1,35 @@
 import axios from "axios";
+import { ApiError } from "./ApiError";
 
 // Create an axios instance with default config
-export const api = axios.create({
+const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:55116",
   headers: {
     "Content-Type": "application/json",
   },
   withCredentials: true,
 });
+
+api.interceptors.response.use(
+  (response) => {
+    console.log("here " + response);
+    if (response.data?.error) {
+      throw new ApiError(response.data.error);
+    }
+    return response;
+  },
+  (error) => {
+    console.log(error.response.data.error);
+    if (error.response.data.error != null) {
+      console.log("JAa");
+      throw new ApiError(error.response.data.error);
+    }
+    // Let actual network errors (timeouts, no response, etc.) bubble up
+    return Promise.reject(error);
+  }
+);
+
+export default api;
 
 // // Add request interceptor for auth tokens
 // api.interceptors.request.use((config) => {
