669aae4187fdcc454b5cfaac6d276ddb21948a59
Leif Etter <etter.leif@gmail.com>
2025-07-14 13:41:58 +0200

    show progress bar to giver overview of generate image progress

---
+++ src/components/ProgressBar/ProgressBarSteps.ts
@@ -0,0 +1,17 @@
+export const PROGRESS_BAR_STEPS = [
+  {
+    label: "Schule",
+    description: "Über Eure Schule",
+    status: "FINISHED",
+  },
+  {
+    label: "Persönliches",
+    description: "Über Dich",
+    status: "IN_PROGRESS",
+  },
+  {
+    label: "Pullover",
+    description: "Pullover-Model Auswahl",
+    status: "NOT_STARTED",
+  },
+];
