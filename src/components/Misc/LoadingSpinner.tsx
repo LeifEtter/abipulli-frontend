5b566c7a791132cb89720f4e0c35a916c76e1ac4
Leif Etter <etter.leif@gmail.com>
2025-07-14 13:24:11 +0200

    change bg color of loading spinner

--- src/components/Misc/LoadingSpinner.tsx
+++ src/components/Misc/LoadingSpinner.tsx
@@ -23,7 +23,7 @@ export const LoadingSpinner = () => (
 export const LoadingSpinnerNew = () => (
   <div className="grid min-h-[140px] w-full place-items-center overflow-x-scroll rounded-lg p-6 lg:overflow-visible">
     <svg
-      className="text-gray-300 animate-spin"
+      className="text-white animate-spin"
       viewBox="0 0 64 64"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
