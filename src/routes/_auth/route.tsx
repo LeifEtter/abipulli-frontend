9ef77d0334f18e49d7a03ca9fe6c721549e14984
Leif Etter <etter.leif@gmail.com>
2025-07-14 13:43:04 +0200

    on navigate to _auth and user not logged in redirect to /onboarding

--- src/routes/_auth/route.tsx
+++ src/routes/_auth/route.tsx
@@ -1,9 +1,20 @@
-import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { createFileRoute, Outlet, useNavigate } from "@tanstack/react-router";
+import { useEffect } from "react";
+import { useAuth } from "src/hooks/useAuth";
 
 export const Route = createFileRoute("/_auth")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const { user, isLoading } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isLoading && user == null) {
+      navigate({ to: "/onboarding" });
+    }
+  }, []);
+
   return <Outlet />;
 }
