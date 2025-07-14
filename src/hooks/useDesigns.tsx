77664a90f756f02b9f7fc92210c8f534ac4aec87
Leif Etter <etter.leif@gmail.com>
2025-07-14 23:11:01 +0200

    switch from order id to user id in useDesigns hook

--- src/hooks/useDesigns.tsx
+++ src/hooks/useDesigns.tsx
@@ -2,17 +2,18 @@ import { Design } from "abipulli-types";
 import { useEffect, useState } from "react";
 import { DesignApi } from "src/api/endpoints/design";
 
-export const useDesigns = (orderId: number) => {
+export const useDesigns = (userId: number | undefined) => {
   const [designs, setDesigns] = useState<Design[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchDesigns = async () => {
+      if (!userId) return;
       setError(null);
       setIsLoading(true);
       try {
-        const designs: Design[] = await DesignApi.retrieveOrderDesigns(orderId);
+        const designs: Design[] = await DesignApi.retrieveAllDesigns(userId);
         setDesigns(designs);
         setIsLoading(false);
       } catch (error) {
@@ -23,7 +24,7 @@ export const useDesigns = (orderId: number) => {
     };
 
     fetchDesigns();
-  }, [orderId]);
+  }, [userId]);
 
   return { designs, designsAreLoading: isLoading, designsError: error };
 };
