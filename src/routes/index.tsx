3b03b21642ce76ddc9db708b1332bbf8201f0292
Leif Etter <etter.leif@gmail.com>
2025-07-14 13:47:46 +0200

    configure / route to redirect to appropriate route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,24 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
+import { useEffect } from "react";
+import { useAuth } from "src/hooks/useAuth";
 
 export const Route = createFileRoute("/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const { user, isLoading } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isLoading) {
+      if (user) {
+        navigate({ to: "/designer" });
+      } else {
+        navigate({ to: "/onboarding" });
+      }
+    }
+  }, [user, isLoading]);
+
   return <div>Hello "/"!!</div>;
 }
