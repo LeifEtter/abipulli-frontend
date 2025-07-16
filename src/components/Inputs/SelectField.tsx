5a0c963c813b7c554504eb02487491b788f9eda7
Leif Etter <etter.leif@gmail.com>
2025-07-16 23:09:24 +0200

    add type to select field component

--- src/components/Inputs/SelectField.tsx
+++ src/components/Inputs/SelectField.tsx
@@ -1,28 +1,28 @@
 import Select, { ActionMeta, SingleValue } from "react-select";
 
-export interface SelectOption {
+export interface SelectOption<T> {
   label: string;
-  value: string;
+  value: T;
 }
 
-interface SelectFieldProps {
+interface SelectFieldProps<T> {
   label: string;
-  options: SelectOption[];
-  chosenOption: SelectOption;
+  options: SelectOption<T>[];
+  chosenOption: SelectOption<T>;
   onChange: (
-    newValue: SingleValue<SelectOption>,
-    actionMeta: ActionMeta<SelectOption>
+    newValue: SingleValue<SelectOption<T>>,
+    actionMeta: ActionMeta<SelectOption<T>>
   ) => void;
   className?: string;
 }
 
-export const SelectField = ({
+export const SelectField = <T,>({
   label,
   options,
   chosenOption,
   onChange,
   className,
-}: SelectFieldProps) => (
+}: SelectFieldProps<T>) => (
   <div className={className}>
     <label htmlFor="select-country-code" className="text-lg font-medium">
       {label}
