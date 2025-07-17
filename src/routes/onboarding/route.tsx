ed3b6553ab27c605cbc63edbcadaaad0a6c6b649
Leif Etter <etter.leif@gmail.com>
2025-07-17 23:56:21 +0200

    remove unsued import

--- src/routes/onboarding/route.tsx
+++ src/routes/onboarding/route.tsx
@@ -1,6 +1,5 @@
 import { createFileRoute, Outlet, useNavigate } from "@tanstack/react-router";
 import { useEffect } from "react";
-import { useOnboardingInfo } from "src/hooks/useOnboardingInfo";
 import { OnboardingProvider } from "src/providers/onboardingProvider";
 
 export const Route = createFileRoute("/onboarding")({
