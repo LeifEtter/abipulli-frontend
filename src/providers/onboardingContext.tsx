e0c01e825370a9d5b47e639a633720813b3de10d
Leif Etter <etter.leif@gmail.com>
2025-07-18 04:08:53 +0200

    redefine functions on OnboardingContext to reflect changes in provider

--- src/providers/onboardingContext.tsx
+++ src/providers/onboardingContext.tsx
@@ -6,20 +6,19 @@ export interface OnboardingContextType {
   userInfo: Partial<User>;
   setUserInfo: (e: Partial<User>) => void;
   userErrors: UserErrors;
-  clearUserError: (key: keyof UserErrors) => void;
 
   orderInfo: Partial<Order>;
   setOrderInfo: (e: Partial<Order>) => void;
   orderErrors: OrderErrors;
-  clearOrderError: (key: keyof OrderErrors) => void;
 
-  // saveProgressLocally: (state: Partial<OnboardingInfo>) => void;
-  submitProgress: () => void;
-  getProgress: () => void;
-  // saveToLocalStorage: () => void;
-  // retrieveFromLocalStorage: () => void;
-  submitUserInfo: () => void;
-  submitOrderInfo: () => void;
+  validateUserInfo: () => boolean;
+  validateOrderInfo: () => boolean;
+
+  submitRegister: () => Promise<void>;
+  submitOrder: () => Promise<void>;
+
+  saveOrderInfoToLocalStorage: () => void;
+  retrieveOrderInfoFromLocalStorage: () => void;
 }
 
 export const OnboardingContext = createContext<OnboardingContextType | null>(
