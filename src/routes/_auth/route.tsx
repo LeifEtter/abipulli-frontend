2d4e6c9e7702ea12deb944a28becba1a8724b8a8
Leif Etter <etter.leif@gmail.com>
2025-08-05 23:36:17 +0200

    show error if auth fails in route.tsx and show popup if backend unavailable

--- src/routes/_auth/route.tsx
+++ src/routes/_auth/route.tsx
@@ -1,16 +1,23 @@
 import { createFileRoute, Outlet, useNavigate } from "@tanstack/react-router";
+import { errorMessages } from "abipulli-types";
 import { useEffect } from "react";
 import { useAuth } from "src/hooks/useAuth";
+import { useSnackbar } from "src/hooks/useSnackbar";
 
 export const Route = createFileRoute("/_auth")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const { user, isLoading } = useAuth();
+  const { user, isLoading, error } = useAuth();
   const navigate = useNavigate();
+  const showSnackbar = useSnackbar();
 
   useEffect(() => {
+    if (error) {
+      if (error.message == errorMessages.backendConnectionError)
+        showSnackbar({ message: "Backend nicht erreichbar", type: "error" });
+    }
     if (!isLoading && user == null) {
       navigate({ to: "/onboarding" });
     }
