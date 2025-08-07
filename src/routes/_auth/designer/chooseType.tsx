74c5bd58b398822e6b0c3ff31293cc8ddbe0f4e0
Leif Etter <etter.leif@gmail.com>
2025-08-07 23:30:30 +0200

    add some identifiers in chooseType file

--- src/routes/_auth/designer/chooseType.tsx
+++ src/routes/_auth/designer/chooseType.tsx
@@ -1,9 +1,32 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { MediumLabel } from "src/components/Texts/MediumLabel";
+import { PageDescription } from "src/components/Texts/PageDescription";
+import { PageTitle } from "src/components/Texts/PageTitle";
 
 export const Route = createFileRoute("/_auth/designer/chooseType")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/_auth/designer/chooseType"!</div>;
+  return (
+    <div className="card">
+      <PageTitle>Wähle ein Pullover Typ/Farbe fürs neue Design</PageTitle>
+      <PageDescription>
+        Dies kannst du selbstverständlich später im Designer ändern
+      </PageDescription>
+      <div className="flex flex-row gap-2">
+        <MediumLabel text="Normal" />
+        <div className="shadow-abipulli-xs border-2 border-abipulli-offblack rounded-md font-semibold bg-abipulli-green px-1">
+          30€
+        </div>
+      </div>
+
+      <div className="flex flex-row font-bold gap-2">
+        <MediumLabel text="Heavy" />
+        <div className="shadow-abipulli-xs border-2 border-abipulli-offblack rounded-md font-semibold bg-abipulli-green px-1">
+          40€
+        </div>
+      </div>
+    </div>
+  );
 }
