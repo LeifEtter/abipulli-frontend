798ed1f687e8daf21ef6af9efe88179adbc5d61b
Leif Etter <etter.leif@gmail.com>
2025-08-13 00:34:16 +0200

    show placeholder in vorschau screen if no images were generated

--- src/routes/_auth/vorschau/index.tsx
+++ src/routes/_auth/vorschau/index.tsx
@@ -1,7 +1,10 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
+import { BasicButton } from "src/components/Buttons/BasicButton";
 import { PageDescription } from "src/components/Texts/PageDescription";
 import { PageTitle } from "src/components/Texts/PageTitle";
+import { SmallLabel } from "src/components/Texts/SmallLabel";
 import { useUserImages } from "src/hooks/useUserImages";
+import { ButtonType } from "src/types/ButtonType";
 
 export const Route = createFileRoute("/_auth/vorschau/")({
   component: RouteComponent,
@@ -21,6 +24,21 @@ function RouteComponent() {
         Bildern.
       </PageDescription>
       <section className="flex gap-4 mt-12 flex-wrap" aria-label="Bilderliste">
+        {userImages.length == 0 && (
+          <div className="flex flex-col items-start">
+            <SmallLabel
+              text="Sieht aus als hättest du noch keine Bilder generiert"
+              className="mb-4"
+            />
+            <BasicButton
+              className="mb-10"
+              type={ButtonType.Link}
+              to={"/generieren"}
+            >
+              Bild Generieren
+            </BasicButton>
+          </div>
+        )}
         {userImages.map((image, id) => (
           <Link
             key={`pick-vorschau-${image.id}`}
