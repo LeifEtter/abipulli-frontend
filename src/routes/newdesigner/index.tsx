9bf2301e79798733e14eb51d807e67ec7951a752
Leif Etter <etter.leif@gmail.com>
2025-08-26 18:50:38 +0200

    setup basic new designer page

--- src/routes/newdesigner/index.tsx
+++ src/routes/newdesigner/index.tsx
@@ -1,9 +1,111 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { JSX, useState } from "react";
+import NameIcon from "src/assets/icons/name-icon.svg";
+import TextIcon from "src/assets/icons/text-icon.svg";
+import PulloverIcon from "src/assets/icons/pullover-icon.svg";
+import ImageIcon from "src/assets/icons/image-icon.svg";
+import AbipulliHat from "src/assets/icons/abipulli-logo.png";
+import BackArrow from "src/assets/icons/back-arrow-icon.svg";
+import FrontArrow from "src/assets/icons/front-arrow-icon.svg";
+import { Center } from "src/components/Misc/Center";
 
 export const Route = createFileRoute("/newdesigner/")({
   component: RouteComponent,
 });
 
+interface SidebarIconProps {
+  selected?: boolean;
+  iconSource: string;
+  label: string;
+}
+
+const SidebarIcon = ({
+  selected = false,
+  iconSource,
+  label,
+}: SidebarIconProps): JSX.Element => (
+  <div className="flex flex-col items-center gap-1">
+    <div className="border border-abipulli-dark-beige h-12 w-12">
+      <Center>
+        <img src={iconSource} className="w-8/12 h-8/12" />
+      </Center>
+    </div>
+    <p className="text-xs font-medium">{label}</p>
+  </div>
+);
+
+interface ToolButtonProps {
+  iconSource: string;
+  label: string;
+  onClick: () => void;
+}
+
+const ToolButton = ({ iconSource, label, onClick }: ToolButtonProps) => (
+  <div className="flex flex-row items-center border-abipulli-darker-beige border rounded-lg hover:scale-105 hover:bg-gray-800 hover:text-white duration-125 hover:border-black cursor-pointer ">
+    <img
+      src={iconSource}
+      className="border-r border-abipulli-darker-beige p-2"
+    />
+    <p className="py-1 px-2 font-medium">{label}</p>
+  </div>
+);
+
 function RouteComponent() {
-  return <div></div>;
+  const [tabSelected, setTabSelected] = useState<string>("My Generated Images");
+
+  return (
+    <div className="flex flex-row h-full w-full">
+      <section
+        id="sidebar"
+        className="h-full w-4/12 shadow-abipulli-sidebar bg-abipulli-light-beige flex flex-row"
+      >
+        <div
+          id="button-section"
+          className="h-full flex flex-col items-center gap-4 pt-8 p-4"
+        >
+          <img src={AbipulliHat} className="w-8" />
+          <SidebarIcon iconSource={PulloverIcon} label="Pullover" />
+          <SidebarIcon iconSource={ImageIcon} label="Bilder" />
+          <SidebarIcon iconSource={TextIcon} label="Texte" />
+          <SidebarIcon iconSource={NameIcon} label="Namen" />
+        </div>
+        <div className="w-full px-6 py-6">
+          <div className="flex flex-row">
+            <button
+              className="w-1/2 cursor-pointer"
+              onClick={() => setTabSelected("My Generated Images")}
+            >
+              <p className="text-left font-semibold">Meine Bilder</p>
+            </button>
+            <button
+              className="w-1/2 cursor-pointer"
+              onClick={() => setTabSelected("Library")}
+            >
+              <p className="text-left font-semibold">Library</p>
+            </button>
+          </div>
+          <div
+            className={`relative h-0.5 w-full top-1 flex flex-row  ${tabSelected == "My Generated Images" ? "justify-start" : "justify-end"} duration-100 mt-2`}
+          >
+            <div className={`w-6/12 bg-gray-600 duration-100 z-20`} />
+            <div className="bg-gray-200 w-full absolute h-0.5" />
+          </div>
+        </div>
+      </section>
+      <section className="flex flex-col w-full">
+        <div className="w-full h-16 bg-white border-b-2 border-b-abipulli-gray flex items-center px-4 gap-4">
+          <ToolButton
+            iconSource={BackArrow}
+            label="Rückgängig"
+            onClick={() => {}}
+          />
+          <ToolButton
+            iconSource={FrontArrow}
+            label="Wiederholen"
+            onClick={() => {}}
+          />
+        </div>
+      </section>
+    </div>
+  );
 }
