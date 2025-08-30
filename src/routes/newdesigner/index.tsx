b2b281a82f6b15b17398c6f345cb23b7b6cbf3e5
Leif Etter <etter.leif@gmail.com>
2025-08-30 23:55:51 +0200

    add example editable text

--- src/routes/newdesigner/index.tsx
+++ src/routes/newdesigner/index.tsx
@@ -147,7 +147,18 @@ function RouteComponent() {
               switchViewingSide={(side: ViewingSide) => setViewingSide(side)}
             />
           </div>
-          <div className="flex justify-center items-center grow ">
+          <div className="flex justify-center items-center grow border">
+            <div
+              draggable
+              contentEditable
+              className="absolute border min-w-30 empty:before:content-[attr(data-placeholder)] empty:before:text-gray-400 empty:before:pointer-events-none empty:before:absolute whitespace-pre-wrap"
+              data-placeholder="asdasd"
+              role="textbox"
+              aria-label="text"
+              onSelect={() => {}}
+              onInput={() => {}}
+              style={{}}
+            />
             <img src={ExamplePullover} alt="" className="max-h-160" />
           </div>
           <div
