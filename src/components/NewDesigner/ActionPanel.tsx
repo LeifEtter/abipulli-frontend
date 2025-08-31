caa7437dd75a0647593d903f809964e4d38b5c50
Leif Etter <etter.leif@gmail.com>
2025-08-31 23:48:51 +0200

    add description to actionpanel and allow passing title/description

--- src/components/NewDesigner/ActionPanel.tsx
+++ src/components/NewDesigner/ActionPanel.tsx
@@ -2,11 +2,18 @@ import { JSX } from "react";
 
 interface ActionPanelProps {
   children: JSX.Element[] | JSX.Element;
+  title: string;
+  description: string;
 }
 
-export const ActionPanel = ({ children }: ActionPanelProps) => (
+export const ActionPanel = ({
+  children,
+  title,
+  description,
+}: ActionPanelProps) => (
   <div className="rounded-xl bg-white border-10 w-50 h-50 duration-100 shadow-ap-special-shadow p-2">
-    <h3>Text Styling</h3>
+    <h3>{title}</h3>
+    <p>{description}</p>
     <div className="w-full h-0.5 bg-gray-200 mt-2" />
     {children}
   </div>
