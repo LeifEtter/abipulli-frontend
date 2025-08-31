745311d4187d4ad3420820d07cbda421d8f4fe1a
Leif Etter <etter.leif@gmail.com>
2025-08-31 23:51:01 +0200

    improve styling of action panel

--- src/components/NewDesigner/ActionPanel.tsx
+++ src/components/NewDesigner/ActionPanel.tsx
@@ -11,9 +11,9 @@ export const ActionPanel = ({
   title,
   description,
 }: ActionPanelProps) => (
-  <div className="rounded-xl bg-white border-10 w-50 h-50 duration-100 shadow-ap-special-shadow p-2">
-    <h3>{title}</h3>
-    <p>{description}</p>
+  <div className="rounded-xl bg-abipulli-light-beige w-50 h-50 duration-100 shadow-ap-special-shadow p-6 min-w-sm">
+    <h3 className="font-semibold text-xl">{title}</h3>
+    <p className="text-md">{description}</p>
     <div className="w-full h-0.5 bg-gray-200 mt-2" />
     {children}
   </div>
