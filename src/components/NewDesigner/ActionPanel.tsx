4359f8beea13f51a8879344785a595f685594258
Leif Etter <etter.leif@gmail.com>
2025-09-04 22:17:32 +0200

    make ActionPanel layout to flex-col

--- src/components/NewDesigner/ActionPanel.tsx
+++ src/components/NewDesigner/ActionPanel.tsx
@@ -14,7 +14,7 @@ export const ActionPanel = ({
   <div className="rounded-xl bg-abipulli-light-beige duration-100 shadow-ap-special-shadow p-6 min-w-sm max-w-lg">
     <h3 className="font-semibold text-2xl text-abipulli-black">{title}</h3>
     <p className="text-md">{description}</p>
-    <div className="w-full h-0.5 bg-gray-200 mt-2" />
+    <div className="w-full h-0.5 bg-gray-200 mt-2 flex flex-col" />
     {children}
   </div>
 );
