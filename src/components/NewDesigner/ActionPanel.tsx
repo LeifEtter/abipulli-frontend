7510304b3f61a749a7250e685dbd9ef1719a4860
Leif Etter <etter.leif@gmail.com>
2025-09-05 23:44:45 +0200

    reduce max size for action panels

--- src/components/NewDesigner/ActionPanel.tsx
+++ src/components/NewDesigner/ActionPanel.tsx
@@ -11,7 +11,7 @@ export const ActionPanel = ({
   title,
   description,
 }: ActionPanelProps) => (
-  <div className="rounded-xl bg-abipulli-light-beige duration-100 shadow-ap-special-shadow p-6 min-w-sm max-w-lg">
+  <div className="rounded-xl bg-abipulli-light-beige duration-100 shadow-ap-special-shadow p-6 min-w-sm max-w-md">
     <h3 className="font-semibold text-2xl text-abipulli-black">{title}</h3>
     <p className="text-md">{description}</p>
     <div className="w-full h-0.5 bg-gray-200 mt-2 flex flex-col" />
