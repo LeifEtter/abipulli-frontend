dc42710af1d3d777a9c0cb2b6679aa8d54a2faaf
Leif Etter <etter.leif@gmail.com>
2025-09-15 19:20:18 +0200

    move image retrieving functionality into callback and expose as refetch

--- src/hooks/useUserImages.tsx
+++ src/hooks/useUserImages.tsx
@@ -1,11 +1,12 @@
 import { Image } from "abipulli-types";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { ImageApi } from "src/api/endpoints/image";
 
 interface UseUserImagesReturn {
   userImages: Image[];
   userImagesAreLoading: boolean;
   userImagesError: string | null;
+  refetch: () => Promise<void>;
 }
 
 export const useUserImages = (): UseUserImagesReturn => {
@@ -13,31 +14,32 @@ export const useUserImages = (): UseUserImagesReturn => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
-  useEffect(() => {
-    const fetchUserImages = async () => {
-      setError(null);
-      setIsLoading(true);
-      try {
-        const images: Image[] = await ImageApi.fetchUsersImages();
-        images.sort(
-          (a, b) =>
-            new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
-        );
-        setUserImages(images);
-        setIsLoading(false);
-      } catch (error) {
-        setUserImages([]);
-        setError("Couldn't fetch User Images");
-        setIsLoading(false);
-      }
-    };
-
-    fetchUserImages();
+  const fetchUserImages = useCallback(async () => {
+    setError(null);
+    setIsLoading(true);
+    try {
+      const images: Image[] = await ImageApi.fetchUsersImages();
+      images.sort(
+        (a, b) =>
+          new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+      );
+      setUserImages(images);
+      setIsLoading(false);
+    } catch (error) {
+      setUserImages([]);
+      setError("Couldn't fetch User Images");
+      setIsLoading(false);
+    }
   }, []);
+
+  useEffect(() => {
+    fetchUserImages();
+  }, [fetchUserImages]);
 
   return {
     userImages,
     userImagesAreLoading: isLoading,
     userImagesError: error,
+    refetch: fetchUserImages,
   };
 };
