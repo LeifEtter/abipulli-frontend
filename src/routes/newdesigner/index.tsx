24f95dd10240cf304c7591582c8609cda1cb5d34
Leif Etter <etter.leif@gmail.com>
2025-09-15 19:23:25 +0200

    general improvements in designer

--- src/routes/newdesigner/index.tsx
+++ src/routes/newdesigner/index.tsx
@@ -1,20 +1,15 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { useEffect, useState } from "react";
-import AbipulliHat from "src/assets/icons/abipulli-logo.png";
-import ExamplePullover from "src/assets/pullovers/sand-front.png";
 import { TabOption, TabSwitcher } from "src/components/NewDesigner/TabSwitcher";
 import { FrontBackButton } from "src/components/Buttons/FrontBackButton";
 import { ViewingSide } from "src/types/ViewingSide";
 import { SelectOption } from "src/components/Inputs/SelectField";
-import { Layer, Rect, Stage, Text } from "react-konva";
-import {
-  ResizableImage,
-  StaticImage,
-} from "src/components/Designer/CanvasImages";
 import { ChooseReferenceImage } from "src/components/NewDesigner/ImageGenActionPanel/ChooseReference";
-import { ImagesTab } from "src/components/NewDesigner/Tabs/ImagesTab";
+import {
+  ImagesTab,
+  ImageTabs,
+} from "src/components/NewDesigner/Tabs/ImagesTab";
 import { DesignsBar } from "src/components/NewDesigner/DesignsBar";
-import { EditableTextField } from "src/components/NewDesigner/EditableTextField";
 import { Toolbar } from "src/components/NewDesigner/Toolbar";
 import { SidebarNav } from "src/components/NewDesigner/SidebarNav";
 import { GenerateInfoProvider } from "src/providers/generateProvider";
@@ -34,6 +29,8 @@ import { PositionType } from "src/types/canvas/positionType";
 import { ScaleType } from "src/types/canvas/scaleType";
 import { NewDesignerCanvas } from "src/components/NewDesigner/NewDesignerCanvas";
 import { useUserImages } from "src/hooks/useUserImages";
+import { DropHereIcon } from "src/assets/icons/drop-here-icon";
+import { ImageApi } from "src/api/endpoints/image";
 
 export const Route = createFileRoute("/newdesigner/")({
   component: RouteComponent,
@@ -44,15 +41,15 @@ function RouteComponent() {
 
   const [imageIsUploading, setImageIsUploading] = useState<boolean>(false);
 
+  const [imageTabSelected, setImageTabSelected] = useState<ImageTabs>(
+    ImageTabs.USER
+  );
+
   const tabs: TabOption[] = [
     { id: 0, label: "Meine Bilder" },
     { id: 1, label: "Bibliothek" },
   ];
   const [tabSelected, setTabSelected] = useState<TabOption>(tabs[0]);
-
-  const images: { src: string; label: string }[] = [
-    { src: AbipulliHat, label: "Image69" },
-  ];
 
   const [viewingSide, setViewingSide] = useState<ViewingSide>(
     ViewingSide.Front
@@ -73,12 +70,6 @@ function RouteComponent() {
   const nextGenerateTab = () => setGenerateTab((prev) => prev! + 1);
   const previousGenerateTab = () =>
     setGenerateTab((prev) => (prev == 0 ? 0 : prev! - 1));
-
-  // const [viewingImage, setViewingImage] = useState<Image>(
-  //   ImageFactory.image({})
-  // );
-
-  const [viewingImage, setViewingImage] = useState<Image>();
 
   const [selectedImage, selectImage] = useState<ImageWithPositionAndScale>();
 
@@ -214,14 +205,20 @@ function RouteComponent() {
         <Toolbar zoom={zoom} setZoom={setZoom} />
         <div
           className={`w-full bg-cover flex flex-col justify-between relative h-full dotted-background`}
+          onClick={(e) => e.currentTarget == e.target && selectImage(undefined)}
         >
           <FrontBackButton
             className="mt-2 ml-2"
             currentViewingSide={viewingSide}
             switchViewingSide={(side: ViewingSide) => setViewingSide(side)}
           />
-          <div className="flex justify-start items-center grow border">
-            <EditableTextField />
+          <div
+            className="flex ml-0 lg:ml-20 xl:ml-30 items-center grow"
+            onClick={(e) =>
+              e.currentTarget == e.target && selectImage(undefined)
+            }
+          >
+            {/* <EditableTextField /> */}
             <NewDesignerCanvas
               designCanvasSize={designCanvasSize}
               viewingSide={viewingSide}
