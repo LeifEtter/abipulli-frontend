17a6fb9ac8a3aced978035f32d34c85e22f1e41f
Leif Etter <etter.leif@gmail.com>
2025-09-18 23:44:47 +0200

    adapt scroll viw in ImagesTab

--- src/components/NewDesigner/Tabs/ImagesTab.tsx
+++ src/components/NewDesigner/Tabs/ImagesTab.tsx
@@ -7,6 +7,14 @@ import DeleteIcon from "src/assets/icons/trash-icon.svg";
 import EditIcon from "src/assets/icons/edit-icon.svg";
 import { Center } from "src/components/Misc/Center";
 
+const ACCEPTED_IMAGE_FORMATS = {
+  "image/jpeg": [],
+  "image/png": [],
+  "image/webp": [],
+  "image/heic": [],
+  "image/jfif": [],
+};
+
 export enum ImageTabs {
   USER = "Deine Bilder",
   GENERAL = "Bibliothek",
@@ -14,7 +22,6 @@ export enum ImageTabs {
 
 interface ImagesTabProps {
   userImages: Image[];
-  generateImage: () => void;
   addImage: (image: Image) => void;
   isDraggingOver: boolean;
   setIsDraggingOver: (isDraggingOver: boolean) => void;
@@ -23,11 +30,11 @@ interface ImagesTabProps {
   imageTabChoice: ImageTabs;
   deleteImage: (imageId: number) => Promise<void>;
   selectImage: (image: Image) => void;
+  className: string;
 }
 
 export const ImagesTab = ({
   userImages,
-  generateImage,
   addImage,
   isDraggingOver,
   setIsDraggingOver,
@@ -36,89 +43,64 @@ export const ImagesTab = ({
   imageTabChoice = ImageTabs.USER,
   deleteImage,
   selectImage,
+  className,
 }: ImagesTabProps) => (
   <Dropzone
-    accept={{
-      "image/jpeg": [],
-      "image/png": [],
-      "image/webp": [],
-      "image/heic": [],
-      "image/jfif": [],
-    }}
+    accept={ACCEPTED_IMAGE_FORMATS}
     maxFiles={5}
     maxSize={10000000}
     onDropAccepted={onDropAccepted}
-    // onDropAccepted={async (files) => {
-    //   setIsDraggingOver(false);
-    //   setImageIsUploading(true);
-    //   await ImageApi.upload(files[0]);
-    //   setImageIsUploading(false);
-    // }}
     onDragOver={() => !isDraggingOver && setIsDraggingOver(true)}
     onDragLeave={() => isDraggingOver && setIsDraggingOver(false)}
   >
     {({ getRootProps, getInputProps }) => (
-      <section className="w-full h-full relative">
-        <div
-          {...getRootProps()}
-          onClick={(e) => e.preventDefault()}
-          className="h-full"
-        >
-          <input {...getInputProps()} />
-          <div>
-            <button
-              onClick={() => generateImage()}
-              className="cursor-pointer bg-white border border-black w-full rounded-xl text-center font-semibold p-3"
-            >
-              Bild Generieren
-            </button>
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mt-4">
-              {imageTabChoice == ImageTabs.USER ? (
-                userImages.map((image, idx) => (
-                  <div>
-                    <div
-                      key={`image-tab-${idx}`}
-                      className="cursor-pointer border-12 border-white aspect-square rounded-md overflow-hidden"
-                      onClick={() => addImage(image)}
-                    >
-                      <img
-                        className="object-cover w-full h-full"
-                        src={image.url}
-                      />
-                    </div>
-                    <div className="mt-2 flex flex-row justify-center items-center gap-1.5">
-                      <p>Image42</p>
-                      <div className="grow" />
-                      <div
-                        className="h-6 w-6 bg-white rounded-md cursor-pointer"
-                        onClick={() => selectImage(image)}
-                      >
-                        <Center>
-                          <img src={EditIcon} className="w-4 h-4" />
-                        </Center>
-                      </div>
-
-                      <div
-                        className="h-6 w-6 bg-white rounded-md cursor-pointer"
-                        onClick={() => deleteImage(image.id)}
-                      >
-                        <Center>
-                          <img src={DeleteIcon} className="w-4 h-4" />
-                        </Center>
-                      </div>
-                    </div>
-                  </div>
-                ))
-              ) : (
-                <></>
-              )}
-              {imageIsUploading && (
-                <div className="bg-gray-200 rounded-md flex flex-col items-center justify-center gap-2 border-dashed border-2 border-gray-300 aspect-square">
-                  <LoadingSpinner className="fill-abipulli-green-strong dark:text-gray-300" />
+      <section
+        {...getRootProps()}
+        className={className}
+        onClick={(e) => e.preventDefault()}
+      >
+        <input {...getInputProps()} />
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+          {imageTabChoice == ImageTabs.USER ? (
+            userImages.map((image, idx) => (
+              <div key={`image-tab-${idx}`}>
+                <div
+                  onClick={() => addImage(image)}
+                  className="cursor-pointer border-12 border-white aspect-square rounded-md overflow-hidden"
+                >
+                  <img className="object-cover w-full h-full" src={image.url} />
                 </div>
-              )}
+                <div className="mt-2 flex flex-row justify-center items-center gap-1.5">
+                  <p>Bild</p>
+                  <div className="grow" />
+                  <div
+                    className="h-6 w-6 bg-white rounded-md cursor-pointer"
+                    onClick={() => selectImage(image)}
+                  >
+                    <Center>
+                      <img src={EditIcon} className="w-4 h-4" />
+                    </Center>
+                  </div>
+
+                  <div
+                    className="h-6 w-6 bg-white rounded-md cursor-pointer"
+                    onClick={() => deleteImage(image.id)}
+                  >
+                    <Center>
+                      <img src={DeleteIcon} className="w-4 h-4" />
+                    </Center>
+                  </div>
+                </div>
+              </div>
+            ))
+          ) : (
+            <></>
+          )}
+          {imageIsUploading && (
+            <div className="bg-gray-200 rounded-md flex flex-col items-center justify-center gap-2 border-dashed border-2 border-gray-300 aspect-square">
+              <LoadingSpinner className="fill-abipulli-green-strong dark:text-gray-300" />
             </div>
-          </div>
+          )}
         </div>
       </section>
     )}
