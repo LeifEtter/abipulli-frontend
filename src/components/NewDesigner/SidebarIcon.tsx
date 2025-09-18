536d36e94df7d5efc362a7dacba9b36b9197b54f
Leif Etter <etter.leif@gmail.com>
2025-09-18 23:45:25 +0200

    Make SidebarIcon buttons responsive

--- src/components/NewDesigner/SidebarIcon.tsx
+++ src/components/NewDesigner/SidebarIcon.tsx
@@ -1,25 +1,29 @@
 import { JSX } from "react";
 import { Center } from "../Misc/Center";
+import { SidebarTab } from "src/routes/newdesigner/route";
 
 interface SidebarIconProps {
   selected?: boolean;
-  iconSource: string;
-  label: string;
+  tab: SidebarTab;
+  callback: (tab: SidebarTab) => void;
 }
 
 export const SidebarIcon = ({
   selected = false,
-  iconSource,
-  label,
+  tab,
+  callback,
 }: SidebarIconProps): JSX.Element => (
-  <div className="flex flex-col items-center gap-1 cursor-pointer">
+  <div
+    className="flex flex-col items-center gap-1 cursor-pointer"
+    onClick={() => callback(tab)}
+  >
     <div
       className={`h-12 w-12 rounded-md ${selected && "bg-abipulli-gray border-1 border-gray-500"}`}
     >
       <Center>
-        <img src={iconSource} className="w-8/12 h-8/12" />
+        <img src={tab.icon} className="w-8/12 h-8/12" />
       </Center>
     </div>
-    <p className="text-xs font-medium">{label}</p>
+    <p className="text-sm font-semibold -mt-0.5">{tab.label}</p>
   </div>
 );
