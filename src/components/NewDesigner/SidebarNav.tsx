6cbad9964d9934c15266a3ad1a68f0262c5f464a
Leif Etter <etter.leif@gmail.com>
2025-09-18 23:45:17 +0200

    Move SidebarTab Data out of Sidebar

--- src/components/NewDesigner/SidebarNav.tsx
+++ src/components/NewDesigner/SidebarNav.tsx
@@ -1,55 +1,34 @@
 import { SidebarIcon } from "./SidebarIcon";
-import NameIcon from "src/assets/icons/name-icon.svg";
-import TextIcon from "src/assets/icons/text-icon.svg";
-import PulloverIcon from "src/assets/icons/pullover-icon.svg";
-import ImageIcon from "src/assets/icons/image-icon.svg";
+
 import AbipulliHat from "src/assets/icons/abipulli-logo.png";
 import { useState } from "react";
+import { SidebarTab } from "src/routes/newdesigner/route";
+import { Divider } from "../Misc/Divider";
 
-export interface SidebarTab {
-  label: string;
-  icon: string;
-  selected?: boolean;
+interface SidebarNavProps {
+  callback: (tab: SidebarTab) => void;
+  tabs: SidebarTab[];
+  tabSelected: SidebarTab;
 }
 
-const tabs: SidebarTab[] = [
-  {
-    label: "Pullover",
-    icon: PulloverIcon,
-  },
-  {
-    label: "Bilder",
-    icon: ImageIcon,
-  },
-  {
-    label: "Texte",
-    icon: TextIcon,
-  },
-  {
-    label: "Namen",
-    icon: NameIcon,
-  },
-];
-
-export const SidebarNav = () => {
-  const [sidebarTabs, setSidebarTabs] = useState<SidebarTab[]>(
-    tabs.map((tab) => ({ ...tab, selected: false }))
-  );
-
-  return (
-    <div
-      id="button-section"
-      className="h-full flex flex-col items-center gap-4 pt-8 p-4"
-    >
-      <img src={AbipulliHat} className="w-8" />
-      {sidebarTabs.map((tabs) => (
-        <SidebarIcon
-          key={`sidebar-button-${tabs.label}`}
-          iconSource={tabs.icon}
-          label={tabs.label}
-          selected={tabs.selected}
-        />
-      ))}
-    </div>
-  );
-};
+export const SidebarNav = ({
+  callback,
+  tabs,
+  tabSelected,
+}: SidebarNavProps) => (
+  <div
+    id="button-section"
+    className="h-full flex flex-col items-center gap-4 pt-8 p-4"
+  >
+    <img src={AbipulliHat} className="w-8" />
+    <Divider className="border border-gray-400" />
+    {tabs.map((tab) => (
+      <SidebarIcon
+        key={`sidebar-button-${tab.label}`}
+        tab={tab}
+        selected={tab == tabSelected}
+        callback={callback}
+      />
+    ))}
+  </div>
+);
