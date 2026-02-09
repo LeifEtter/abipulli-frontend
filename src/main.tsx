103d119b4ad3cef969f49bdbf61345db5e6b4070
Leif Etter <etter.leif@gmail.com>
2026-02-09 05:00:02 +0100

    add react query provider to top of route tree

--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,34 @@ import "./index.css";
 import "./utilities/index";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthProvider } from "./providers/authProvider";
 import { SnackbarProvider } from "./providers/snackbarProvider";
 import { RouterContextWrapper } from "./RouterContextWrapper";
 import { PopupProvider } from "./providers/popupProvider";
 
+// Create a client
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000, // 5 minutes
+      gcTime: 30 * 60 * 1000, // 30 minutes (formerly cacheTime)
+      refetchOnWindowFocus: false,
+      retry: 1,
+    },
+  },
+});
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AuthProvider>
-      <SnackbarProvider>
-        <PopupProvider>
-          <RouterContextWrapper />
-        </PopupProvider>
-      </SnackbarProvider>
-    </AuthProvider>
-  </StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <SnackbarProvider>
+          <PopupProvider>
+            <RouterContextWrapper />
+          </PopupProvider>
+        </SnackbarProvider>
+      </AuthProvider>
+    </QueryClientProvider>
+  </StrictMode>,
 );
