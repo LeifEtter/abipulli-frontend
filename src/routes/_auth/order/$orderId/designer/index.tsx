bfc13e1e55ce19f22bb639bb773724a9fcef00e8
Leif Etter <etter.leif@gmail.com>
2026-02-16 23:53:22 +0100

    remove automatic redirect and add titles for created abipullis

--- src/routes/_auth/order/$orderId/designer/index.tsx
+++ src/routes/_auth/order/$orderId/designer/index.tsx
@@ -8,15 +8,15 @@ export const Route = createFileRoute("/_auth/order/$orderId/designer/")({
       parseInt(params.orderId),
     );
 
-    if (designs.length > 0) {
-      throw redirect({
-        to: "/order/$orderId/designer/$designId/pullover",
-        params: {
-          orderId: params.orderId,
-          designId: designs[0].id.toString(),
-        },
-      });
-    }
+    // if (designs.length > 0) {
+    //   throw redirect({
+    //     to: "/order/$orderId/designer/$designId/pullover",
+    //     params: {
+    //       orderId: params.orderId,
+    //       designId: designs[0].id.toString(),
+    //     },
+    //   });
+    // }
 
     return { designs };
   },
@@ -26,9 +26,8 @@ export const Route = createFileRoute("/_auth/order/$orderId/designer/")({
 function RouteComponent() {
   return (
     <div className="flex items-center justify-center h-full w-full">
-      <p className="text-lg text-gray-600">
-        No designs found. Please create a design first.
-      </p>
+      <h2>Deine Entwürfe</h2>
+      <h2>Neuen AbiPulli erstellen</h2>
     </div>
   );
 }
